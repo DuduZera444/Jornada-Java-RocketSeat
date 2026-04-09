d41c27e559e78aecb860e191e994aa1c74b5ade5
Eduardo Fontenele <g0ddudu17@gmail.com>
2026-04-09 18:27:41 -0300

    Sintaxe java: Variaveis

---
+++ src/com/rocketseat/sintaxe/Variaveis.java
@@ -0,0 +1,18 @@
+package com.rocketseat.sintaxe;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		        String BR = "Brasil";
+		        double PI = 3.14;
+		        int ESTADOS_BRASILEIRO = 27;
+		        int ANO_2000 = 2000;
+
+		        /* As variaveis são espaços na memoria para guardar dados que podem ser requisitados
+		        ou modificados, 
+		         */
+
+
+	}
+
+}
