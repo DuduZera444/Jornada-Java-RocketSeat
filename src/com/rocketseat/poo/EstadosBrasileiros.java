2afdcb04e630af2ebb47f4419fb5f9c0ae0063ec
Eduardo Fontenele <g0ddudu17@gmail.com>
2026-04-17 23:41:28 -0300

    feat declarando enums

---
+++ src/com/rocketseat/poo/EstadosBrasileiros.java
@@ -0,0 +1,17 @@
+package com.rocketseat.poo;
+
+public enum EstadosBrasileiros {
+	PI,
+	MA,
+	CE,
+	SP,
+	RS,
+	RJ,
+	SC
+	
+	/* Enums é um tipo especial de classe e serve para representar algum conjunto de constantes.
+	 * no exemplo acima é mostrado a abreviação de alguns estados brasileiros, que foram declar
+	 * ados como enums.
+	 * */
+	
+}
